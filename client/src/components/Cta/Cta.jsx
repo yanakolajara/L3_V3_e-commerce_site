010b1b79679aa41e73d00e0bb57de977a31a292b
yanakolajara <yanakolajara@pursuit.org>
2025-05-20 18:43:01 -0400

    navbar main design

--- client/src/components/Cta/Cta.jsx
+++ client/src/components/Cta/Cta.jsx
@@ -1,11 +1,68 @@
-export const Cta = ({ renderIcon, children, ...props }) => {
+/**
+ *
+ * @param {
+ * className?: string,
+ * children?: React.ReactNode,
+ * iconPosition?: 'left' | 'right',
+ * border?: boolean,
+ * borderColor?: string,
+ * borderWidth?: string,
+ * borderRadius?: string,
+ * size?: 'md' | 'sm',
+ * color?: string,
+ * backgroundColor?: string,
+ * onClick?: () => void,
+ * href?: string,
+ * target?: string,
+ * renderIcon?: React.ReactNode,
+ * } props
+ * @returns {JSX.Element}
+ */
+export const Cta = ({
+  className,
+  children,
+  iconPosition = 'left',
+  border = false,
+  borderColor = 'black',
+  borderWidth = '1px',
+  borderRadius = '5px',
+  size = 'md',
+  color = 'black',
+  backgroundColor = 'transparent',
+  onClick,
+  href,
+  renderIcon,
+  ...props
+}) => {
   const style = {
+    border: border ? `${borderWidth} solid ${borderColor}` : 'none',
+    borderRadius,
+    color,
+    backgroundColor,
+    fontSize: size === 'md' ? '16px' : '12px',
+    padding: size === 'md' ? '10px 20px' : '5px 10px',
     cursor: 'pointer',
   };
+  if (href) {
+    return (
+      <a className={className} href={href} style={style} {...props}>
+        {iconPosition === 'left' && renderIcon}
+        {children}
+        {iconPosition === 'right' && renderIcon}
+      </a>
+    );
+  }
   return (
-    <div className='cta' style={style} {...props}>
-      {renderIcon && renderIcon()}
+    <button
+      className={className}
+      onClick={onClick}
+      href={href}
+      style={style}
+      {...props}
+    >
+      {iconPosition === 'left' && renderIcon}
       {children}
-    </div>
+      {iconPosition === 'right' && renderIcon}
+    </button>
   );
 };
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Cta } from '../Cta/Cta';
 import { VscSearch } from 'react-icons/vsc';
+import { FaRegHeart } from 'react-icons/fa';
+import { LuShoppingCart } from 'react-icons/lu';
+
 import './Navbar.css';
 import logo from '../../assets/ReverbLogo.svg';
 
@@ -21,12 +24,47 @@ export function Navbar() {
         </form>
       </div>
       <div className='navbar-options'>
-        <Cta>Sell Your Gear</Cta>
-        <Cta>Favorites</Cta>
-        <Cta>Cart</Cta>
+        <Cta border borderColor='black' borderWidth='3px' borderRadius='25px'>
+          Sell Your Gear
+        </Cta>
+        <button
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            background: 'none',
+            border: 'none',
+          }}
+        >
+          <FaRegHeart
+            style={{ color: 'black', fontSize: '1.5rem' }}
+          ></FaRegHeart>
+          Favorites
+        </button>
 
-        <Cta>Sign Up</Cta>
-        <Cta>Log In</Cta>
+        <button
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            background: 'none',
+            border: 'none',
+          }}
+        >
+          <LuShoppingCart
+            style={{ color: 'black', fontSize: '1.5rem' }}
+          ></LuShoppingCart>
+          Cart
+        </button>
+
+        <Cta href='/signup' size='sm'>
+          Sign Up
+        </Cta>
+        <Cta href='/login' size='sm'>
+          Log In
+        </Cta>
       </div>
     </nav>
   );
